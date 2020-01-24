e86667c3163e17d8a282f677d28e90c8dcd79ad2
Patrick Marabeas <patrick.marabeas@anz.com>
2020-01-24 14:49:03 +1100

    Add Github Terraform Provider and Github App Installation auth flow
    - Add Terraform Provider
    - Auth flow to generate short lived Github App Installation token
    - Instantiate  GraphQL and REST clients for calls to the Github APIs

---
+++ github/provider.go
@@ -0,0 +1,122 @@
+package github
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	p := &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			PROVIDER_BASE_URL: {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("GITHUB_BASE_URL", "https://api.github.com/"),
+				Description: "The GitHub Root API URL.",
+			},
+			PROVIDER_ORGANIZATION: {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("GITHUB_ORGANIZATION", nil),
+				Description: "The target GitHub organization to manage.",
+			},
+			PROVIDER_TOKEN: {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("GITHUB_TOKEN", nil),
+				Description: "The GitHub access token.",
+				Sensitive:   true,
+			},
+			PROVIDER_APP: {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						PROVIDER_APP_PEM: {
+							Type:        schema.TypeString,
+							Required:    true,
+							DefaultFunc: schema.EnvDefaultFunc("GITHUB_APP_PEM", nil),
+							Description: "The GitHub App PEM string.",
+							Sensitive:   true,
+						},
+						PROVIDER_APP_ID: {
+							Type:        schema.TypeString,
+							Required:    true,
+							DefaultFunc: schema.EnvDefaultFunc("GITHUB_APP_ID", nil),
+							Description: "The GitHub App ID.",
+						},
+						PROVIDER_APP_INSTALLATION_ID: {
+							Type:        schema.TypeString,
+							Required:    true,
+							DefaultFunc: schema.EnvDefaultFunc("GITHUB_APP_INSTALLATION_ID", nil),
+							Description: "The GitHub App installation instance ID.",
+						},
+					},
+				},
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+		},
+	}
+
+	p.ConfigureFunc = providerConfigure(p)
+
+	return p
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
+	return func(d *schema.ResourceData) (interface{}, error) {
+		var (
+			baseURL      = d.Get(PROVIDER_BASE_URL).(string)
+			organization = d.Get(PROVIDER_ORGANIZATION).(string)
+			token        = d.Get(PROVIDER_TOKEN).(string)
+			appPEM       = ""
+			appID        = ""
+			appInstID    = ""
+		)
+
+		if v, ok := d.GetOk(PROVIDER_APP); ok {
+			vL := v.([]interface{})
+			if len(vL) > 1 {
+				return nil, fmt.Errorf("error: multiple %s declarations", "app")
+			}
+			for _, v := range vL {
+				if v == nil {
+					break
+				}
+
+				m := v.(map[string]interface{})
+				if v, ok := m[PROVIDER_APP_PEM]; ok {
+					appPEM = v.(string)
+				}
+				if v, ok := m[PROVIDER_APP_ID]; ok {
+					appID = v.(string)
+				}
+				if v, ok := m[PROVIDER_APP_INSTALLATION_ID]; ok {
+					appInstID = v.(string)
+				}
+			}
+		}
+
+		config := Config{
+			BaseURL:        baseURL,
+			Organization:   organization,
+			Token:          token,
+			Pem:            appPEM,
+			AppID:          appID,
+			InstallationID: appInstID,
+		}
+
+		meta, err := config.Clients()
+		if err != nil {
+			return nil, err
+		}
+
+		meta.(*Organization).StopContext = p.StopContext()
+
+		return meta, nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/patrickmarabeas/terraform-provider-githubv4
+
+go 1.13
+
+require (
+	github.com/SermoDigital/jose v0.9.2-0.20161205224733-f6df55f235c2
+	github.com/google/go-github/v28 v28.1.1
+	github.com/hashicorp/terraform v0.12.19
+	github.com/shurcooL/githubv4 v0.0.0-20191127044304-8f68eb5628d0
+	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+)
