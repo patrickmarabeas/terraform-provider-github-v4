62cb3ebd9f7c6d9a02f4140aa046e5580c8288e1
Patrick Marabeas <patrick.marabeas@anz.com>
2020-01-24 15:07:29 +1100

    Add repository collaborators Terraform Data Source

--- github/util_repository.go
+++ github/util_repository.go
@@ -1,7 +1,8 @@
 package github
 
 const (
-	CODEOWNERS_EXISTS = "exists"
-	REPOSITORY_ID 	  = "repository_id"
-	REPOSITORY_NAME   = "repository_name"
+	CODEOWNERS_EXISTS        = "exists"
+	REPOSITORY_COLLABORATORS = "collaborators"
+	REPOSITORY_ID            = "repository_id"
+	REPOSITORY_NAME          = "name"
 )
