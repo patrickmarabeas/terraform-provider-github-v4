6704630d86e74e274e135a10f86ac5405c71e9f7
Patrick Marabeas <patrick.marabeas@anz.com>
2020-01-24 14:59:57 +1100

    Add repository Terraform Data Source

---
+++ github/data_source_github_repository.go
@@ -0,0 +1,48 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/shurcooL/githubv4"
+)
+
+func dataSourceGithubRepository() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+
+		Schema: map[string]*schema.Schema{
+			// Input
+			REPOSITORY_NAME: {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "",
+			},
+		},
+
+		Read: dataSourceGithubRepositoryRead,
+	}
+}
+
+func dataSourceGithubRepositoryRead(d *schema.ResourceData, meta interface{}) error {
+	var query struct {
+		Repository struct {
+			ID githubv4.ID
+		} `graphql:"repository(owner:$owner, name:$name)"`
+	}
+	variables := map[string]interface{}{
+		"owner": githubv4.String(meta.(*Organization).Name),
+		"name":  githubv4.String(d.Get("name").(string)),
+	}
+
+	ctx := context.Background()
+	client := meta.(*Organization).GraphQLClient
+	err := client.Query(ctx, &query, variables)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(fmt.Sprintf("%s", query.Repository.ID))
+
+	return nil
+}
